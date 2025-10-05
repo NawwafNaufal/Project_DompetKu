41e48eb13e9415b082332538a8939e5926788fc3
NawwafNaufal <nawwafnaufal03@gmail.com>
2025-10-05 11:25:08 +0700

    add pagination redis and db

--- Backend/src/controller/admin/getDataTransaction.ts
+++ Backend/src/controller/admin/getDataTransaction.ts
@@ -4,10 +4,16 @@ import {getUsersService} from "../../service/admin/dataUsers"
 export const getDataController = async (req:Request,res:Response,next:NextFunction) => {
 
     const userId = res.locals.user
+    const take = Number(req.query.data) 
+    const page = Number(req.query.page) || 1
 
     try {        
-        const result = await getUsersService (userId.id)
+        const result = await getUsersService (userId.id,take,page)
             return res.status(200).json({
+                take,
+                page,
+                totalPage : Math.ceil(Number(result.total) / take),
+                totalData : result.total,
                 data : result.data,
                 source : result.source
             })
--- Backend/src/service/admin/dataUsers.ts
+++ Backend/src/service/admin/dataUsers.ts
@@ -1,25 +1,31 @@
 import redis from "../../../config/redis";
 import { PrismaClient } from "@prisma/client";
 
-export const getUsersService = async (userId : string) => {
-    console.log(userId)
+export const getUsersService = async (userId : string, take : number, page : number) => {
+
+    const skip = (page -1) * take
+
     const prisma = new PrismaClient()
-        const cacheKey = userId ? `transactions:${userId}` : "transactions:all";
+        const cacheKey = `transactions:${userId}-page:${page}-take:${take}`
         
             const getFromRedis = await redis.get(cacheKey)
             if(getFromRedis){
-                 const parse = JSON.parse(getFromRedis as string)
-                 return {data : parse , source : "cache"}
+                const parse = JSON.parse(getFromRedis as string)
+                return {data : parse , source : "Cache",total: parse.totalData}
             }
 
             const getFromDb = await prisma.transaction.findMany({
                 where : {
                     userId : userId as string
-                }
+                },
+                skip : skip,
+                take : take
             })
-        
-            await redis.setex(cacheKey, 3600, JSON.stringify(getFromDb));
 
-            return {data : getFromDb , source : "db"}
+            const totalData = await prisma.transaction.count()
+        
+            await redis.setex(cacheKey, 3600, JSON.stringify({getFromDb,totalData}));
+
+            return {data : getFromDb , source : "Db",total : totalData}
 }
 
